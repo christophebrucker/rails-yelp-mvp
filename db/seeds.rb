4527f9b2a251c4ce6378b604cd302eb4c4cffda7
Christophe Brucker <christophe.brucker@gmail.com>
2020-09-09 15:37:51 +0200

    Yelp MVP

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,31 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+
+puts 'Cleaning database...'
+Restaurant.destroy_all
+Review.destroy_all
+
+puts 'Creating restaurants...'
+category = %w[chinese italian japanese french japanese belgian]
+
+50.times do
+  restaurant = Restaurant.create!(
+    name: Faker::Restaurant.name,
+    address: Faker::Address.full_address,
+    phone_number: Faker::PhoneNumber.cell_phone_with_country_code,
+    category: category.sample
+  )
+  3.times do
+    Review.create!(
+      rating: rand(0..5),
+      content: Faker::Restaurant.review,
+      restaurant_id: restaurant.id
+    )
+  end
+end
+
+puts 'Finished for the restaurants!'
+
+puts 'Finished for the reviews!'
